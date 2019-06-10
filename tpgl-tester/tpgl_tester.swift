2902f48151a35aa1224d75cf7887c7cd3f33e25a
sebug <sebastian.gfeller@gmail.com>
2019-06-10 12:18:44 +0200

    First succeeding test

--- tpgl-tester/tpgl_tester.swift
+++ tpgl-tester/tpgl_tester.swift
@@ -8,6 +8,8 @@
 import XCTest
 
 class tpgl_tester: XCTestCase {
+    
+    var application: XCUIApplication?
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -16,7 +18,8 @@ class tpgl_tester: XCTestCase {
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication(bundleIdentifier: "ch.sebug.tpgl").launch()
+        application = XCUIApplication(bundleIdentifier: "ch.sebug.tpgl");
+        application!.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -28,7 +31,8 @@ class tpgl_tester: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
+        let windows = application!.windows
+        XCTAssertEqual(2, windows.count)
     }
 
 }
